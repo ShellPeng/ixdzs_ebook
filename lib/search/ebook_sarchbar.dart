ae16e0ceba8f85dc354868e5e0a676c12e35d570
ShellPeng <peng@shellunion.com>
2019-05-25 18:24:31 +0800

    搜索

---
+++ lib/search/ebook_sarchbar.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:floating_search_bar/floating_search_bar.dart';
+import '../common/screen.dart';
+
+class EbookSearchBar extends StatelessWidget {
+  EbookSearchBar({
+    this.body,
+    this.drawer,
+    this.trailing,
+    this.leading,
+    this.endDrawer,
+    this.controller,
+    this.onChanged,
+    this.title,
+    this.decoration,
+    this.onTap,
+    @required List<Widget> children,
+  }) : _childDelagate = SliverChildListDelegate(
+          children,
+        );
+
+  EbookSearchBar.builder({
+    this.body,
+    this.drawer,
+    this.endDrawer,
+    this.trailing,
+    this.leading,
+    this.controller,
+    this.onChanged,
+    this.title,
+    this.onTap,
+    this.decoration,
+    @required IndexedWidgetBuilder itemBuilder,
+    @required int itemCount,
+  }) : _childDelagate = SliverChildBuilderDelegate(
+          itemBuilder,
+          childCount: itemCount,
+        );
+
+  final Widget leading, trailing, body, drawer, endDrawer;
+
+  final SliverChildDelegate _childDelagate;
+
+  final TextEditingController controller;
+
+  final ValueChanged<String> onChanged;
+
+  final InputDecoration decoration;
+
+  final VoidCallback onTap;
+
+  /// Override the search field
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: drawer,
+      endDrawer: endDrawer,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+              pinned: true,
+              floating: false,
+              title: Text('搜索',style:TextStyle(color: Colors.white)),
+              expandedHeight:Screen.topSafeHeight+20,
+              flexibleSpace: CustomScrollView(
+                physics: NeverScrollableScrollPhysics(),
+                slivers: <Widget>[
+                  SliverFloatingBar(
+                    leading: leading,
+                    floating: false,
+                    title: TextField(
+                      controller: controller,
+                      decoration: decoration ??
+                          InputDecoration.collapsed(
+                            hintText: "Search...",
+                          ),
+                      autofocus: false,
+                      onChanged: onChanged,
+                      onTap: onTap,
+                    ),
+                    trailing: trailing,
+                    pinned: true,
+                  ),
+                ],
+              )),
+
+          SliverList(
+            delegate: _childDelagate,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/search/search_page.dart
+++ lib/search/search_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:floating_search_bar/floating_search_bar.dart';
+import 'ebook_sarchbar.dart';
 
 class SearchPage extends StatelessWidget {
   
@@ -6,53 +8,63 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('搜索', style: TextStyle(color: Colors.white))),
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-            SearchInput()
-          ],
-        ),
-      ),
-    );
-  }
-}
+      // appBar: AppBar(title: Text('搜索', style: TextStyle(color: Colors.white))),
 
 
-///搜索框
-class SearchInput extends StatelessWidget {
-  // final getResults;
-
-  // final ValueChanged<String> onSubmitted;
-
-  // final VoidCallback onSubmitPressed;
-
-  // SearchInput(this.getResults, this.onSubmitted, this.onSubmitPressed);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      height: 40.0,
-      decoration: BoxDecoration(
-          color: Theme.of(context).backgroundColor,
-          borderRadius: BorderRadius.circular(4.0)),
-      child: new Row(
+      body: EbookSearchBar(
+        leading: Icon(Icons.search),
+        decoration:InputDecoration.collapsed(hintText: "搜索"),
+        // onChanged: (keyword){
+        //   print('搜索关键词：'+keyword);
+        // },
+        // onTap: (){
+        //   print('被点击');
+        // },
         children: <Widget>[
-          new Padding(
-            padding: new EdgeInsets.only(right: 10.0, top: 3.0, left: 10.0),
-            child: new Icon(Icons.search,
-                size: 24.0, color: Theme.of(context).accentColor),
-          ),
-          new Expanded(
-            child: new TextField(
-              // placeholder: '搜索 flutter 组件',
-              // getResults: getResults,
-            ),
-          ),
+ 
         ],
       ),
+
+
+      // body: FloatingSearchBar.builder(
+      //   leading: Icon(Icons.search),
+      //   decoration:InputDecoration.collapsed(hintText: "搜索"),
+      //   itemCount: 100,
+      //   itemBuilder: (BuildContext context, int index) {
+      //     return ListTile(title: Text('${index}'),);
+      //   },
+      //   onChanged: (keyword){
+      //     print('搜索关键词：'+keyword);
+      //   },
+      //   onTap: (){
+      //     print('被点击');
+      //   },
+      // ),
+
+
+      // body: CustomScrollView(
+      //   physics: NeverScrollableScrollPhysics(),
+      //   slivers: <Widget>[
+      //   SliverPadding(padding: EdgeInsets.only(top: 15),),
+      //   SliverFloatingBar(
+      //       leading: Icon(Icons.search),
+      //       floating: false,
+      //       title: TextField(
+      //             decoration:InputDecoration.collapsed(hintText: "Search..."),
+      //             autofocus: false,
+      //             onTap: (){
+
+      //             },
+      //           ),
+      //     ),
+      // ],),
+
+      
     );
   }
+
+  
 }
-// wigdet干掉.=> componets
+
+
 
